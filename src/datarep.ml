9923563dfb8344c2e8791efeca0f5898c9f8955b
treyzania <treyzania@gmail.com>
2018-03-07 18:42:52 -0500

    Further fleshed out mid-level data represenation, adding full support for funcs and dealing with arguments.

--- src/datarep.ml
+++ src/datarep.ml
@@ -16,25 +16,74 @@ type isn_arg =
 	| Blob of string (* This is for when we don't know what is going on, like
 						in x86 most of the time. *)
 	| Literal of value
-	| Argument of string (* Function arguments, when we support that. *)
+	| Register of string
+
+(* Loading a value from an argument. *)
+type argload_stmt = {
+	arg_name : string ;
+	destination : string
+}
+
+(* Storing a value back in as an argument, probably not used a lot. *)
+type argstore_stmt = {
+	arg_name : string ;
+	source : string
+}
+
+(* Returning values, via the stack. *)
+type return_stmt = {
+	source : isn_arg option
+}
 
 (* Statements that are just literal instructions. *)
 type instruction_stmt = {
 	isn_name : string ;
-	prefixes : string list ;
+	prefixes : string list ; (* x86 has a "lock" prefix, etc. *)
 	arguments : isn_arg list
 }
 
+(* A block of multiple statements. *)
+type stmt_block = statement list
+
 (* Some imperative statement in a block. *)
-type statement =
+and statement =
 	| Nop
+	| Block of stmt_block
 	| Instruction of instruction_stmt
-	| Block of statement list
+	| ArgLoad of argload_stmt
+	| ArgStore of argstore_stmt
+	| Return of return_stmt
 
 (* Executable subroutines that emit assembly code. *)
 type subroutine = {
 	name : string ;
-	body : statement list
+	body : stmt_block
+}
+
+(* Calling conventions. *)
+type cconv = string
+
+(* Argument types, only supporting integers for now. *)
+type argtype =
+	| Pointer
+	| U8
+	| U16
+	| U32
+	| U64
+	| Buffer of int (* Arbitrary sized stuff, passed as pointers. *)
+
+(* Arguments to functions. *)
+type func_arg = {
+	name : string ;
+	argtype : argtype
+}
+
+(* Functions are like subroutines, but actually have a calling conventions and take arguments. *)
+type func = {
+	name : string ;
+	calling_convention : cconv ;
+	arguments : func_arg list ;
+	body : stmt_block
 }
 
 (* Definitions inside of a section. *)
